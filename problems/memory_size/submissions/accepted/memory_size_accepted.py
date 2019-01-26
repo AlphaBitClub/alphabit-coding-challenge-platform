6aad4eaefa5bf3d673be32a709c8cfed9e89183d
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-01-26 19:38:46 +0100

    add Python solution for mem_size problem

---
+++ problems/memory_size/submissions/accepted/memory_size_accepted.py
@@ -0,0 +1,19 @@
+n = int(input())
+memSize = 0
+
+
+def findMemorySize(memorySize):
+    if (memorySize & (memorySize - 1)) == 0:
+        return memorySize
+    powerOfTwo = 1
+    while powerOfTwo < memorySize:
+        powerOfTwo <<= 1
+    return powerOfTwo
+
+for i in range(0, n):
+    dataSize = int(input())
+    memSize += dataSize
+
+memSize = findMemorySize(memSize)
+
+print(memSize)
