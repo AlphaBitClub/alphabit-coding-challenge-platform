616be61f34999760983433b998061fc25e4d27dd
philomath213 <bilalphilomath@gmail.com>
2019-02-11 22:31:05 +0100

    fix zeros_accepted.pas

--- problems/zeros/submissions/accepted/zeros_accepted.pas
+++ problems/zeros/submissions/accepted/zeros_accepted.pas
@@ -1,7 +1,7 @@
 program exo1;
-var x,two,five:Integer;n,i:longint;
-function multiplicity( x:integer; y:integer):Integer;
-var count:Integer; 
+var x,two,five:longint;n,i:longint;
+function multiplicity( x:longint; y:longint):longint;
+var count:longint;
 begin
 count:=0;
 while x mod y=0 do
@@ -11,7 +11,7 @@ count:=count+1;
 end;
 multiplicity:=count;
 end;
-  
+
 begin
 readln(n);
 two:=0; five:=0;
@@ -21,8 +21,6 @@ readln(x);
 
 if x mod 2=0 then two:=two+multiplicity(x,2);
 if x mod 5=0 then five:=five+multiplicity(x,5);
-{if two < five then writeln(two) else writeln(five)
-}
 end;
 
 if two<five then writeln(two) else writeln(five);
