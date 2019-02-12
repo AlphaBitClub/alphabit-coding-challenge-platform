6d988c238c655beef442c1d5db8d479e9e3be439
philomath213 <bilalphilomath@gmail.com>
2019-02-12 19:11:36 +0100

    add time limit exceeded Book Giving python solution

---
+++ problems/bookgiving/submissions/time_limit_exceeded/bookgiving_time_limit_exceeded.py
@@ -0,0 +1,31 @@
+from operator import itemgetter
+
+
+# a stack to store the books
+book_stack = []
+
+# first line contains N
+n = int(input())
+
+# for each N lines
+for _ in range(n):
+    # read the querie type and apply each
+    querie = input()
+
+    # Add <book_name> <rating> querie
+    if querie.startswith("Add"):
+        _, book_name, rating = querie.split()
+        rating = int(rating)
+        # push the book in the book_stack
+        book_stack.append((book_name, rating))
+
+    # Give Querie
+    elif querie.startswith("Give"):
+        book_stack.pop()
+
+    # Best Querie
+    elif querie.startswith("Best"):
+        # Naive O(n) Solution: find the the book with the highest rating for each querie
+        best_book = max(book_stack, key=itemgetter(1))
+        # print "<book_name> <rating>"
+        print("{} {}".format(*best_book))
