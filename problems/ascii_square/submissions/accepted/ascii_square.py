810f93e202b0247ccb1b30d80c065381354068ff
philomath213 <bilalphilomath@gmail.com>
2019-11-26 18:59:19 +0100

    add Ascii Square python solution

---
+++ problems/ascii_square/submissions/accepted/ascii_square.py
@@ -0,0 +1,17 @@
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+x = input().strip()
+
+x_index = LETTERS.index(x)
+width = 2*x_index + 1
+
+for i in range(width):
+    line = ''
+    for j in range(width):
+        i_distance = abs(i - x_index)
+        j_distance = abs(j - x_index)
+        c_index = max(i_distance, j_distance)
+        c = LETTERS[c_index]
+        line += c
+
+    print(line)
