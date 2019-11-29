1e167bc8018062b6ca9c951df8b7fe9ff54b3d0c
bibs2091 <m.naas@esi-sba.dz>
2019-11-29 02:27:19 +0100

    add simplfied_tetris problem

---
+++ problems/simplfied_tetris/submissions/accepted/simplfied_tetris.py
@@ -0,0 +1,16 @@
+D = int(input())
+S = int(input())
+N = int(input())
+
+if (N==1):
+    print(S)
+elif((2*D + S) >= N):
+    if ((N % 2) == 0):
+        print( (4*D + S)//N )
+    else:
+        if ( (2*D)>=S ):
+            print(S)
+        else:
+            print( (4*D + S)//N )
+else:
+    print(0)
