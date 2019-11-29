6b15334c65d033cb5c4b9f914a6749d26d9c4917
philomath213 <bilalphilomath@gmail.com>
2019-11-29 23:45:05 +0100

    clean and format ascii_square.pas

--- problems/ascii_square/submissions/accepted/ascii_square.pas
+++ problems/ascii_square/submissions/accepted/ascii_square.pas
@@ -1,46 +1,33 @@
 program ascii_square;
-var i,j,width,x_index,i_distance,j_distance,c_index:integer; x,c:char;line,letters: string;
-
-    function index(letters:string;letter:char): integer;
-        var i:integer;found:boolean;
-        begin
-            i := 0 ;
-            found := false;
-            while  ((i < 27)and (not found))  do
-            begin
-                i := i + 1;
-                if (letters[i] = letter) then
-                found := true  ;
-            end;
-            index := i;
-        end;
+var i, j, width, x_index, i_distance, j_distance, c_index: integer;
+    x, c: char;
+    line, letters: string;
         
-    function max(num1:integer;num2:integer): integer;
-        begin
-            if (num1>num2) then
-            max:= num1
-            else
+function max(num1:integer; num2:integer): integer;
+    begin
+        if (num1 > num2) then
+            max := num1
+        else
             max := num2;
-        end;
+    end;
 
 begin
     letters := 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     readln(x);
-    x_index := index(letters,x);
-    width := 2 * (x_index -1) +1;
+    x_index := ord(x) - ord('A') + 1;
+    width := 2 * (x_index -1) + 1;
 
-    for i:=1 to width do
+    for i := 1 to width do
     begin
-       line:='';
-       for j:=1 to width do
+       line := '';
+       for j:= 1 to width do
        begin
             i_distance := abs(i - x_index);
             j_distance := abs(j - x_index);
-            c_index := max(i_distance,j_distance);
+            c_index := max(i_distance, j_distance);
             c := letters[c_index+1];
             line := line + c;
        end;
        writeln(line);
     end;
 end.
-
