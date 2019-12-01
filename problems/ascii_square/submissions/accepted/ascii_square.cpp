014b7c01768465a6a33d8755bc0b23fa1100ca3e
ilovax <tasfaoutabderrahimilovax@gmail.com>
2019-12-01 02:00:56 +0200

    adding c++ solution for ascii_square problem

---
+++ problems/ascii_square/submissions/accepted/ascii_square.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+	using namespace std;
+
+int main()
+{
+	string LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char x ;
+	cin >> x;
+
+	int x_index = LETTERS.find(x);
+	int width = 2*x_index + 1;
+
+	string line;
+	int i_distance,j_distance,c_index;
+	char c;
+
+	for(int i=0;i<width;i++){
+		line ="";
+		for (int j = 0; j < width; j++)
+		{
+			i_distance = abs(i - x_index);
+        	j_distance = abs(j - x_index);
+	        c_index = max(i_distance, j_distance);
+	        c = LETTERS[c_index];
+	        line += c;
+		}
+		cout << line<< endl;
+	}
+	return 0;
+}
