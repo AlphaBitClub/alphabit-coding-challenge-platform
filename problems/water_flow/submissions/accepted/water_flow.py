0e8d1c7d3b451b2725ed95fba9b960669f905c14
philomath213 <bilalphilomath@gmail.com>
2019-12-12 02:30:20 +0100

    fix water flow test cases names

--- problems/water_flow/submissions/accepted/water_flow.py
+++ problems/water_flow/submissions/accepted/water_flow.py
@@ -1,4 +1,4 @@
-#getting the needed data
+# getting the needed data
 flow = float(input())
 n_valves = int(input())
 
@@ -8,14 +8,14 @@ valves_bandwidth = [int(i) for i in valves_bandwidth_table]
 delays_table = input().strip().split(" ")
 delays = [0]+[int(i) for i in delays_table]
 
-#table of time needed by each valve to distribute  the water flow
-results =[flow/valves_bandwidth[0]]
+# table of time needed by each valve to distribute  the water flow
+results = [flow/valves_bandwidth[0]]
 
-for i in range(1,len(delays)):
-	delays[i] += delays[i-1]
-	results.append(flow/valves_bandwidth[i] +  delays[i])
+for i in range(1, len(delays)):
+    delays[i] += delays[i-1]
+    results.append(flow/valves_bandwidth[i] + delays[i])
 
-#the right answer
+# the right answer
 the_vlave = results.index(min(results))+1
 
 print(the_vlave)
