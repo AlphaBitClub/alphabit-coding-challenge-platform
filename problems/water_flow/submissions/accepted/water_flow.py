c0a3c60f51a35efdb699571bb49a0359401e0881
philomath213 <bilalphilomath@gmail.com>
2019-12-20 18:30:03 +0100

    refactor python solution for Water Flow

--- problems/water_flow/submissions/accepted/water_flow.py
+++ problems/water_flow/submissions/accepted/water_flow.py
@@ -2,20 +2,20 @@
 flow = float(input())
 n_valves = int(input())
 
-valves_bandwidth_table = input().strip().split(' ')
-valves_bandwidth = [int(i) for i in valves_bandwidth_table]
+valves_bandwidth = list(map(int, input().strip().split(' ')))
 
-delays_table = input().strip().split(" ")
-delays = [0]+[int(i) for i in delays_table]
+delays = [0] + list(map(int, input().strip().split(' ')))
 
-# table of time needed by each valve to distribute  the water flow
+# table of time needed by each valve to distribute the water flow
 results = [flow/valves_bandwidth[0]]
 
 for i in range(1, len(delays)):
+    # accumulate the delays
     delays[i] += delays[i-1]
+    # calculate the needed time
     results.append(flow/valves_bandwidth[i] + delays[i])
 
 # the right answer
-the_vlave = results.index(min(results))+1
+the_valve = results.index(min(results))+1
 
-print(the_vlave)
+print(the_valve)
