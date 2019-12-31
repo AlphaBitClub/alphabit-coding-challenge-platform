90130d12e0dbc1b40de65d6ba889b9605b8fdd6a
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-12-31 21:44:09 +0100

    updating C solution for one_dimension problem

--- problems/One_dimension/submissions/accepted/One_dimension.c
+++ problems/One_dimension/submissions/accepted/One_dimension.c
@@ -5,30 +5,11 @@
 int main(int argc, char const *argv[])
 {
 
-    char str[100];
-    int n, m, k, l, count = 0;
-
-    fgets(str, sizeof(str), stdin);
-
-    //couting spaces
-    for (int i = 0; str[i] != '\0'; ++i)
-        if (' ' == str[i])
-            ++count;
-
-    //initializing the array
-    int oneDim[++count];
-
-    //getting the first token
-    char *token = strtok(str, " ");
-
-    //loop throught the string and getting all tokens
-    int j = 0;
-    while (token != NULL)
-    {
-        oneDim[j] = atoi(token);
-        j++;
-        token = strtok(NULL, " ");
-    }
+    int n, m, k, l, count;
+    scanf("%d", &count);
+    int oneDim[count];
+    for (int i = 0; i < count; i++)
+        scanf("%d", &oneDim[i]);
 
     scanf("%d", &n);
     scanf("%d", &m);
