1e059a50c195e976acbaebd4befae63e186928b7
philomath213 <bilalphilomath@gmail.com>
2020-01-01 23:41:25 +0100

    refactor nokia_keypad python solution

--- problems/Nokia_1110_keypad/submissions/accepted/Nokia_keypad.py
+++ problems/Nokia_1110_keypad/submissions/accepted/Nokia_keypad.py
@@ -1,10 +1,19 @@
-letters = [["a","b","c"],["d","e","f"],["g","h","i"],["j","k","l"],["m","n","o"],["p","q","r","s"],["t","u","v"],["w","x","y"]]
+LETTERS = [
+    ["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"],
+    ["m", "n", "o"], ["p", "q", "r", "s"], ["t", "u", "v"], ["w", "x", "y"]
+]
+
 input()
 line = input()
-words = line.split(" ")
+sequence = line.split(" ")
 result = ""
-for word in words:
-    group = letters[int(word[0])-2]
-    result += " " if word == "0" else group[(len(word)-1) % len(group)]
-    
+for letter_code in sequence:
+    if letter_code == "0":
+        result += " "
+    else:
+        group_index = int(letter_code[0])-2
+        group = LETTERS[group_index]
+        letter_index = (len(letter_code)-1) % len(group)
+        result += group[letter_index]
+
 print(result)
