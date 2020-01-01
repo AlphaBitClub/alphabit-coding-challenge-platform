7220d07ba7680dabd98ddac0656dd478da00dfcb
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2020-01-01 22:37:16 +0100

    adding Python solution for Nokia_keypad problem

---
+++ problems/Nokia_1110_keypad/submissions/accepted/Nokia_keypad.py
@@ -0,0 +1,9 @@
+letters = [["a","b","c"],["d","e","f"],["g","h","i"],["j","k","l"],["m","n","o"],["p","q","r","s"],["t","u","v"],["w","x","y"]]
+line = input()
+words = line.split(" ")
+result = ""
+for word in words:
+    group = letters[int(word[0])-2]
+    result += " " if word == "0" else group[(len(word)-1) % len(group)]
+    
+print(result)
