bf43037cdca2820cbefb731f17b45df3ffe0f843
Mou97 <m.taguiouine@esi-sba.dz>
2020-01-02 17:51:50 +0100

    lint python code with flake8 for the problem Star Wars virtuel queue

--- problems/star_wars_virtuel_queue/submissions/accepted/star_wars_virtuel_queue.py
+++ problems/star_wars_virtuel_queue/submissions/accepted/star_wars_virtuel_queue.py
@@ -1,9 +1,8 @@
 n = int(input())
 
-for i in range(1,n+1):
-    print("0"* (n-i) + "1" * i )
+for i in range(1, n + 1):
+    print("0" * (n-i) + "1" * i)
 
-## other solution ; 
+# other solution;
 # for i in  reversed(range(n)):
 #     print ("0" * (i) + "1"*(n-i) )
-
