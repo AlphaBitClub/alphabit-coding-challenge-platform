b63a3f9bac513a99b48d2dea3e0fda7f3e9d65bd
philomath213 <bilalphilomath@gmail.com>
2020-01-04 13:10:14 +0100

    fix Go solution for starwars virtual queue

--- problems/star_wars_virtuel_queue/submissions/accepted/star_wars_virtuel_queue.go
+++ problems/star_wars_virtuel_queue/submissions/accepted/star_wars_virtuel_queue.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
-import "strconv"
+import (
+	"fmt"
+)
 
 func main() {
 	var n int
 	fmt.Scan(&n)
-	str := ""
 	for i := n; i > 0; i-- {
-		str += "1"
-		fmt.Printf("%"+strconv.Itoa(n)+"0\n", str)
+		for x := 1; x < i; x++ {
+			fmt.Print("0")
+		}
+		for y := i; y <= n; y++ {
+			fmt.Print("1")
+		}
+		fmt.Println()
 	}
 }
